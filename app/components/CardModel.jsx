08641df058618f5eaf37eb21db0d06a405f1ec72
WoraponK <worapon.klabsri@gmail.com>
2024-03-19 21:06:24 +0700

    fix:card size

--- app/components/CardModel.jsx
+++ app/components/CardModel.jsx
@@ -31,7 +31,7 @@ const CardModel = ({ name, urlPokemon }) => {
 
     return (
         <>
-            <div onClick={() => handleModal()} className='border-2 rounded p-3 space-y-2 flex justify-center items-center flex-col cursor-pointer transition-all active:scale-95 hover:border-stone-400'>
+            <div onClick={() => handleModal()} className='border-2 rounded p-3 h-[150px] aspect-square space-y-2 flex justify-center items-center flex-col cursor-pointer transition-all active:scale-95 hover:border-stone-400'>
                 <h2 className='text-lg font-semibold capitalize'>{name}</h2>
                 <div className='h-[80px] aspect-square flex justify-center items-center'>
                     <img src={pokemonModel} alt={name} className='h-full w-full'/>
